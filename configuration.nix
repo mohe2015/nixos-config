4955744b5c8dcff5329970f888a3141ef9bae6df
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-09-17 14:54:26 +0200

    wip

--- configuration.nix
+++ configuration.nix
@@ -1,3 +1,8 @@
+# safe impermanence setup: have a list of directories that should be persisting and have a script that checks whether there are files anywhere that are not in that list
+# mount tmpfs or trash btrfs to places that we really dont want to persist (within these could be nested a persistent directory again)
+# maybe another btrfs subvolume for data that can be easily reconstructed like /nix/store. Then we can easily snapshot all the data we find actually useful
+
+
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
@@ -11,9 +16,31 @@
       <home-manager/nixos>
     ];
 
+#boot.kernelParams = ["amd_pstate=passive"];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
     "steam" "steam-original" "steam-run"
   ];
+
+virtualisation.libvirtd.enable = true;
+programs.virt-manager.enable = true;
+
+
+virtualisation.containers.enable = true;
+  virtualisation = {
+    docker.enable = true;
+    #podman = {
+    #  enable = true;##
+
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+   #   dockerCompat = true;
+
+      # Required for containers under podman-compose to be able to talk to each other.
+    #  defaultNetwork.settings.dns_enabled = true;
+    #};
+  };
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
@@ -28,6 +55,8 @@
 
   # Enable networking
   networking.networkmanager.enable = true;
+
+  systemd.oomd.enableUserSlices = true;
 
   # Set your time zone.
   time.timeZone = "Europe/Berlin";
@@ -50,6 +79,10 @@
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
+  services.xserver.videoDrivers = [ "amdgpu" ];
+
+  hardware.amdgpu.amdvlk.enable = true;
+  hardware.amdgpu.amdvlk.support32Bit.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
@@ -92,11 +125,7 @@
   users.users.moritz = {
     isNormalUser = true;
     description = "Moritz Hedtke";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      kdePackages.kate
-    #  thunderbird
-    ];
+    extraGroups = [ "networkmanager" "wheel" "wireshark" ];
   };
   home-manager.useUserPackages = true;
   home-manager.useGlobalPkgs = true;
@@ -111,11 +140,11 @@
           host = "secureimap.t-online.de";
           port = 993;
         };
-        signature.showSignature = "append";
-        signature.text = ''
-          Viele Grüße
-          Moritz Hedtke
-        '';
+        #signature.showSignature = "append";
+        #signature.text = ''
+        #  Viele Grüße
+        #  Moritz Hedtke
+        #'';
         smtp = {
           host = "securesmtp.t-online.de";
           port = 587;
@@ -158,21 +187,33 @@
     };
     programs.bash.enable = true;
     programs.firefox.enable = true;
+    programs.firefox.package = pkgs.firefox-bin;
+    programs.vscode.enable = true;
+    programs.vscode.package = pkgs.vscodium;
+    home.packages = [pkgs.mission-center pkgs.verapdf pkgs.htop pkgs.libreoffice-fresh pkgs.ktorrent pkgs.python3 pkgs.jetbrains.pycharm-community pkgs.jetbrains.idea-community pkgs.tinymist pkgs.inkscape pkgs.rustup pkgs.clang_18 pkgs.pkg-config pkgs.file ];
+    programs.chromium.enable = true;
 
     # The state version is required and should stay at the version you
     # originally installed.
     home.stateVersion = "24.05";
   };
 
+  fonts.fontconfig.enable = true;
+  fonts.packages = [ pkgs.roboto ];
+  fonts.fontDir.enable = true;
+
   services.flatpak.enable = true;
 
   # can break system audio, dont use
   programs.steam = {
-    enable = false;
+    enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
   };
+
+  programs.wireshark.enable = true;
+  programs.wireshark.package = pkgs.wireshark;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
@@ -198,7 +239,7 @@
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
